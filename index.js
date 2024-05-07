674b87a82818b0a56474560beb004119d8f88f63
dygsuhij <gs1boomsmd3@hotmail.com>
2024-05-07 21:04:00 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-function threeSum(nums) {
-  nums.sort((a, b) => a - b);
-  const result = [];
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
-      let low = i + 1;
-      let high = nums.length - 1;
-      const sum = 0 - nums[i];
-      while (low < high) {
-        if (nums[low] + nums[high] === sum) {
-          result.push([nums[i], nums[low], nums[high]]);
-          while (low < high && nums[low] === nums[low + 1]) low++;
-          while (low < high && nums[high] === nums[high - 1]) high--;
-          low++;
-          high--;
-        } else if (nums[low] + nums[high] < sum) low++;
-        else high--;
-      }
+function coinChange(coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
     }
   }
-  return result;
+  return dp[amount] === Infinity ? -1 : dp[amount];
 }

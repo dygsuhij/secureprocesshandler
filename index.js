9edfbf879f6baa9179f59ca6565687870db00c22
dygsuhij <gs1boomsmd3@hotmail.com>
2024-05-12 14:25:22 +0700

    start

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-function combinationSum(candidates, target) {
-  const result = [];
-  backtrack(0, [], 0);
-  return result;
-  function backtrack(start, current, sum) {
-    if (sum === target) {
-      result.push([...current]);
-      return;
-    }
-    if (sum > target || start === candidates.length) return;
-    for (let i = start; i < candidates.length; i++) {
-      current.push(candidates[i]);
-      backtrack(i, current, sum + candidates[i]);
-      current.pop();
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
     }
   }
-}
+  return arr;
+};
